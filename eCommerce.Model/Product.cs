d21e2dbf4e8645b6cb15d7b50b3f8de7d6fd3154
Ryan Mainard <rmainard007@live.com>
2016-01-09 15:52:41 -0800

    original commit

---
+++ eCommerce.Model/Product.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+
+namespace eCommerce.Model
+{
+    public class Product
+    {
+        public int ProductId { get; set; }
+
+        public string Description { get; set; }
+
+        [MaxLength(255)]
+        public string ImageUrl { get; set; }
+
+        public decimal Price { get; set; }
+    }
+}
---
+++ eCommerce.WebUI/Controllers/HomeController.cs
@@ -0,0 +1,34 @@
+﻿using eCommerce.DAL.Repositories;
+using eCommerce.DAL.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace eCommerce.WebUI.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            CustomerRepository customers = new CustomerRepository(new DataContext());
+            ProductRepository products = new ProductRepository(new DataContext());
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+}
